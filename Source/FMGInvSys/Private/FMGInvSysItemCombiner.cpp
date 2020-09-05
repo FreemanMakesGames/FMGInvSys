9167e1e5ca9a522a137e396fa9e2404d7d05f5bb
Freeman <freeman@freemanmakesgames.pro>
2020-09-04 19:46:10 -0800

    Prevent Text_Count on item clicker from blocking mouse clicks; Write copyright notice.

--- Source/FMGInvSys/Private/FMGInvSysItemCombiner.cpp
+++ Source/FMGInvSys/Private/FMGInvSysItemCombiner.cpp
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Copyright 2020 FreemanMakesGames https://www.freemanmakesgames.pro
 
 
 #include "FMGInvSysItemCombiner.h"
